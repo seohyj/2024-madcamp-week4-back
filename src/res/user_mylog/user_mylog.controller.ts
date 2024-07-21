c9ca3fec8ebedcf6c1d01986f61be34de7e661bd
seohyj <linajoo03@gmail.com>
2024-07-21 17:20:51 +0900

    Feat: TypeORM checked for WriteDiary

--- src/res/user_mylog/user_mylog.controller.ts
+++ src/res/user_mylog/user_mylog.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Logger } from '@nestjs/common';
 import { UserMylogService } from './user_mylog.service';
 import { UserMylog } from './user_mylog.entity';
 
@@ -7,12 +7,17 @@ import { UpdateWakeTimeDto } from './dto/update-wake-time.dto';
 
 @Controller('user-mylog')
 export class UserMylogController {
+  private readonly logger = new Logger(UserMylogController.name);
+
   constructor(private readonly userMylogService: UserMylogService) {}
 
   @Post()
   async createUserMylog(
     @Body() createUserMylogDto: CreateUserMylogDto
   ): Promise<UserMylog> {
-    return this.userMylogService.createUserMylog(createUserMylogDto);
+    this.logger.log('Received createUserMylog request', createUserMylogDto);
+    const result = await this.userMylogService.createUserMylog(createUserMylogDto);
+    this.logger.log('Saved user mylog', result);
+    return result;
   }
 }
